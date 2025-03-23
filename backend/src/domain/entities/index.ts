2d56f76e921f86f824c0f6926b39dd0a26da4ea1
Jose Moreno <josemcp30@gmail.com>
2025-03-23 19:06:15 +0100

    Entities barrel file added

---
+++ backend/src/domain/entities/index.ts
@@ -0,0 +1,12 @@
+
+export * from './cashCount.entity';
+export * from './client.entity';
+export * from './incidence.entity';
+export * from './inventory.entity';
+export * from './invoice.entity';
+export * from './order.entity';
+export * from './product.entity';
+export * from './reservation.entity';
+export * from './shipment.entity';
+export * from './technician.entity';
+export * from './user.entity';
