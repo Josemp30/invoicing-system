05ea4c99dd76241ba1536a86f0b65a9ab16d4c6f
Jose Moreno <josemcp30@gmail.com>
2025-03-23 19:05:41 +0100

    Repositories barrel file added

---
+++ backend/src/domain/repositories/index.ts
@@ -0,0 +1,12 @@
+
+export * from './cashCount.repository';
+export * from './client.repository';
+export * from './incidence.repository';
+export * from './inventory.repository';
+export * from './invoice.repository';
+export * from './order.repository';
+export * from './product.repository';
+export * from './reservation.repository';
+export * from './shipment.repository';
+export * from './technician.repository';
+export * from './user.repository';
