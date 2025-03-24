08e1f05a21bc08846aa691f86f1cd81786d1a960
Jose Moreno <josemcp30@gmail.com>
2025-03-24 21:29:49 +0100

    Reservation repository updated

--- backend/src/domain/repositories/reservation.repository.ts
+++ backend/src/domain/repositories/reservation.repository.ts
@@ -1,14 +1,15 @@
+import { CreateReservationDto, UpdateReservationDto } from "../dtos/reservations";
 import { ReservationEntity } from "../entities/reservation.entity";
 
 export abstract class ReservationRepository {
 
-    abstract create() : Promise<ReservationEntity>;
+    abstract create(dto: CreateReservationDto) : Promise<ReservationEntity>;
 
     abstract getAll() : Promise<ReservationEntity[]>;
 
     abstract findById(id: string) : Promise<ReservationEntity>;
 
-    abstract updateById() : Promise<ReservationEntity>;
+    abstract updateById(dto: UpdateReservationDto) : Promise<ReservationEntity>;
 
     abstract deleteById(id: string) : Promise<ReservationEntity>;
 
