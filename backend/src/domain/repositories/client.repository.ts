f6f654953fd8cc2272833083673db13bafe4fe7e
Jose Moreno <josemcp30@gmail.com>
2025-03-24 18:21:08 +0100

    Client repository updated

--- backend/src/domain/repositories/client.repository.ts
+++ backend/src/domain/repositories/client.repository.ts
@@ -1,14 +1,15 @@
+import { CreateClientDto, UpdateClientDto } from "../dtos/clients";
 import { ClientEntity } from "../entities/client.entity";
 
 export abstract class ClientRepository {
 
-    abstract create() : Promise<ClientEntity>;
+    abstract create(dto: CreateClientDto) : Promise<ClientEntity>;
 
     abstract getAll() : Promise<ClientEntity[]>;
 
     abstract findById(id: string) : Promise<ClientEntity>;
 
-    abstract updateById() : Promise<ClientEntity>;
+    abstract updateById(dto: UpdateClientDto) : Promise<ClientEntity>;
 
     abstract deleteById(id: string) : Promise<ClientEntity>;
 
