422dd7d6ce55049f92972d5193a01ea456cc92ef
Jose Moreno <josemcp30@gmail.com>
2025-03-28 16:59:44 +0100

    Barrel files added

---
+++ backend/src/domain/datasources/index.ts
@@ -0,0 +1,13 @@
+
+export * from './cashCount.datasource';
+export * from './client.datasource';
+export * from './incidence.datasource';
+export * from './inventory.datasource';
+export * from './invoice.datasource';
+export * from './order.datasource';
+export * from './product.datasource';
+export * from './reservation.datasource';
+export * from './shipment.datasource';
+export * from './store.datasource';
+export * from './technician.datasource';
+export * from './user.datasource';
--- backend/src/domain/repositories/index.ts
+++ backend/src/domain/repositories/index.ts
@@ -8,5 +8,6 @@ export * from './order.repository';
 export * from './product.repository';
 export * from './reservation.repository';
 export * from './shipment.repository';
+export * from './store.repository';
 export * from './technician.repository';
 export * from './user.repository';
