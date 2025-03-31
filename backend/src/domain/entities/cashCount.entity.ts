8dcfcc7e173d9cb866343bda882a8a6f69c48b91
Jose Moreno <josemcp30@gmail.com>
2025-03-31 22:16:07 +0200

    Cash count entity updated

--- backend/src/domain/entities/cashCount.entity.ts
+++ backend/src/domain/entities/cashCount.entity.ts
@@ -7,6 +7,8 @@ export class CashCountEntity {
         public id: string,
         public date: string,
         public invoices: InvoiceEntity[],
+        public cardTotal: number,
+        public cashTotal: number,
         public totalAmount: number,
     ){}
 
@@ -15,14 +17,18 @@ export class CashCountEntity {
             id,
             date,
             invoices,
+            cardTotal,
+            cashTotal,
             totalAmount,
         } = object;
 
         if(!id) throw CustomError.badRequest('ID is required');
         if(!date) throw CustomError.badRequest('Date is required');
+        if(!cardTotal) throw CustomError.badRequest('Card total amount is required');
+        if(!cashTotal) throw CustomError.badRequest('Cash total amount is required');
         if(!totalAmount) throw CustomError.badRequest('Total amount is required');
 
-        return new CashCountEntity(id, date, invoices, totalAmount);
+        return new CashCountEntity(id, date, invoices, cardTotal, cashTotal, totalAmount);
 
     }
 }
